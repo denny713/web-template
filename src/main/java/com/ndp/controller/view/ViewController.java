bc0e81a1f4d85d43e3813503ab1dca9ea5b78f64
Denny Afrizal <denny.afrizal713@gmail.com>
2024-09-30 15:50:41 +0700

    Login Page

--- src/main/java/com/ndp/controller/view/ViewController.java
+++ src/main/java/com/ndp/controller/view/ViewController.java
@@ -1,7 +1,6 @@
 package com.ndp.controller.view;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,17 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ViewController {
 
     @GetMapping("/login")
-    public String loginPage(Model model) {
-        return render(model, "login :: login", true);
+    public String loginPage() {
+        return "login";
     }
 
     @GetMapping({"/", "/dashboard"})
-    public String dashboard(Model model) {
-        return render(model, "dashboard :: dashboard", false);
-    }
-
-    private String render(Model model, String component, boolean isLogin) {
-        model.addAttribute("view", component);
-        return isLogin ? "layout/sign-layout" : "layout/base-layout";
+    public String dashboard() {
+        return "dashboard";
     }
 }
