eefa549357ec7a0acb98f3847d8a24b79724c5a6
Denny Afrizal <denny.afrizal713@gmail.com>
2024-10-03 17:53:42 +0700

    Change Password from Profile Page

--- src/main/resources/static/assets/js/custom/auth.js
+++ src/main/resources/static/assets/js/custom/auth.js
@@ -10,6 +10,13 @@ function doLogin() {
     }
 }
 
+function changePassExecute() {
+    $("#current-pass-modal").val("");
+    $("#new-pass-modal").val("");
+    $("#confirm-pass-modal").val("");
+    $("#username-modal").val($("#username-profile").val());
+}
+
 function doChangePass() {
     let oldPass = $("#current-pass").val();
     let newPass = $("#new-pass").val();
@@ -36,6 +43,36 @@ function doChangePass() {
 
         confirm("/api/auth/update-pass", "Are yut sure want to change this password?", request, function () {
             doAuth(username, newPass);
+        });
+    }
+}
+
+function doChangePassFromProfile() {
+    let oldPass = $("#current-pass-modal").val();
+    let newPass = $("#new-pass-modal").val();
+    let confirmPass = $("#confirm-pass-modal").val();
+    let username = $("#username-modal").val();
+    if (username === "" || username == null) {
+        username = "-";
+    }
+
+    if (oldPass === "" || oldPass == null) {
+        showNotice('warning', "Warning", "Old password cannot be null or empty");
+    } else if (newPass === "" || newPass == null) {
+        showNotice('warning', "Warning", "New password cannot be null or empty");
+    } else if (confirmPass === "" || confirmPass == null) {
+        showNotice('warning', "Warning", "Confirmation password cannot be null or empty");
+    } else if (oldPass === newPass || oldPass === confirmPass) {
+        showNotice('warning', "Warning", "Old password cannot same as new password");
+    } else if (newPass !== confirmPass) {
+        showNotice('warning', "Warning", "Invalid confirmation password");
+    } else {
+        let request = {};
+        request["oldPassword"] = oldPass;
+        request["newPassword"] = newPass;
+
+        confirm("/api/auth/update-pass", "Are yut sure want to change this password?", request, function () {
+            reload();
         });
     }
 }
